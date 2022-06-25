af98738725f935a922756fdf766e79c046a2df54
sgarciagallego <gallegogarciasergio@gmail.com>
2022-06-25 16:26:51 +0100

    v0.23.0 | [Adding Unicons locally] - removing package

--- src/components/icons/List.js
+++ src/components/icons/List.js
@@ -1,9 +1,27 @@
 import React from 'react'
-import { UilCaretRight } from '@iconscout/react-unicons'
+import styled from 'styled-components'
+
+const IconWrapper = styled.div`
+  width: 14px;
+  height: auto;
+  display: flex;
+  align-items: center;
+  margin-right: 5px;
+`
 
 const IconList = () => {
   return (
-    <UilCaretRight height='0.9rem' color='rgba(200, 200, 200, 1)' />
+    <IconWrapper>
+      <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        viewBox="0 0 24 24"
+      >
+        <path 
+          fill="rgb(175, 175, 175)" 
+          d="M19.5,11.13,5.5,3.05a1,1,0,0,0-1,0,1,1,0,0,0-.5.87V20.08a1,1,0,0,0,.5.87,1,1,0,0,0,1,0l14-8.08a1,1,0,0,0,0-1.74ZM6,18.35V5.65L17,12Z"
+        />
+      </svg>
+    </IconWrapper>
   )
 }
 
